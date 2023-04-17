5cf4f6c2a2f6362799b8145bdbe70b1dd6156351
sindydwns <sindydwns@naver.com>
2023-04-17 17:19:22 +0900

    auth fix

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,9 +1,19 @@
-import { Controller, Get, InternalServerErrorException, Query, Req, Res, UnauthorizedException, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  InternalServerErrorException,
+  Post,
+  Query,
+  Req,
+  Res,
+  UnauthorizedException,
+  UseGuards,
+} from '@nestjs/common';
 import { AuthService } from 'src/auth/auth.service';
 import { UserService } from './user.service';
 import { Request, Response } from 'express';
 import { AuthGuard } from 'src/auth/auth.guard';
-import * as dotenv from "dotenv";
+import * as dotenv from 'dotenv';
 
 @Controller('user')
 export class UserController {
@@ -14,42 +24,49 @@ export class UserController {
     dotenv.config();
   }
 
-  @Get('login')
-  async login(@Req() req, @Res() res) {
+  @Get('auth_page')
+  async authPage(@Req() req, @Res() res) {
     const authSite = `https://api.intra.42.fr/oauth/authorize`;
     const clientId =
       'client_id=u-s4t2ud-dd72647792601b765e45b2925cd5455ca3b035957e70a99241e645fa8d23b4e2';
-    const redir = `redirect_uri=${req.protocol}://${req.headers.host}/user/auth/42/redirect`;
+    console.log(req.headers.referer);
+    const redir = `redirect_uri=${req.headers.referer}auth/42/redirect`;
     const url = `${authSite}?${clientId}&${redir}&response_type=code`;
     res.redirect(url);
   }
 
-  @Get('auth/42/redirect')
-  async redir(@Query('code') code: string, @Res() res: Response) {
+  @Post('login')
+  async login(@Query('code') code: string, @Res() res: Response) {
     const rawUser = await this.authService.getUserInfoFrom42(code);
-    if (rawUser == null)
-      throw new UnauthorizedException(`error`);
+    if (rawUser == null) throw new UnauthorizedException(`error`);
     let user = await this.userService.getUser(rawUser.intraId);
-    if (user == null)
-      user = await this.userService.addUser(rawUser);
-    else
-      this.userService.updateUser(user);
-    if (user == null)
-      throw new InternalServerErrorException('not found user');
+    if (user == null) user = await this.userService.addUser(rawUser);
+    else this.userService.updateUser(user);
+    if (user == null) throw new InternalServerErrorException('not found user');
     const token = await this.authService.createToken(user);
-    res.cookie("access_token", token.access_token, {
+    res.cookie('token', token, {
       maxAge: +process.env.JWT_AGE * 1000,
       httpOnly: true,
-      sameSite: "none",
+      sameSite: 'none',
       secure: true,
     });
-    res.redirect(process.env.CLIENT_DOMAIN);
-    // res.json({ message: "Login success" });
+    res.json({ token });
   }
 
-  @Get("me")
+  @Post('logout')
+  async logout(@Req() req, @Res() res: Response) {
+    res.cookie('token', null, {
+      maxAge: +process.env.JWT_AGE * 1000,
+      httpOnly: true,
+      sameSite: 'none',
+      secure: true,
+    });
+    res.json({ message: 'success logout' });
+  }
+
+  @Get('me')
   @UseGuards(AuthGuard)
   async me(@Req() req: Request) {
-    return req["user"];
+    return req['user'];
   }
 }
