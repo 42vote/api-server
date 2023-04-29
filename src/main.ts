1b3b5e8b5d8e76c8063d07b7a379c902de178f22
sindydwns <sindydwns@naver.com>
2023-04-29 15:19:39 +0900

    fix: rateLimit

--- src/main.ts
+++ src/main.ts
@@ -29,9 +29,9 @@ async function bootstrap() {
   });
   app.use(
     rateLimit({
-      windowMs: 60 * 1000, // 1 minutes
+      windowMs: 10 * 60 * 1000, // 10 minutes
       // max: +process.env.LIMIT_FOR_MIN ?? 100, // Limit each IP to 100 requests per `window` (here, per 1 minutes)
-      max: +configService.get('LIMIT_FOR_MIN') ?? 100,
+      max: +configService.get('LIMIT_FOR_MIN') * 10 ?? 1000,
       standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
       legacyHeaders: false, // Disable the `X-RateLimit-*` headers
     }),
