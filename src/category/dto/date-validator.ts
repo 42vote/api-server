969a755f574b7d5d661261d3583fa45be3d0e8a7
Yangson Andrew Choi <yachoi@Yangsons-MacBook-Pro.local>
2023-06-14 17:47:49 +0900

    FEAT Add isValidDate validator to date string

---
+++ src/category/dto/date-validator.ts
@@ -0,0 +1,20 @@
+import { registerDecorator, ValidationArguments, ValidationOptions } from 'class-validator';
+
+export default function IsValidDate(validationOptions?: ValidationOptions) {
+  return (object: object, propertyName: string) => {
+    registerDecorator({
+      name: 'IsValidDate',
+      target: object.constructor,
+      propertyName,
+      constraints: [],
+      options: validationOptions,
+      validator: {
+        validate: (value: any, args: ValidationArguments) => {
+          const date = new Date(value);
+          return !isNaN(date.getTime());
+        },
+        defaultMessage: () => `Invalid date format`,
+      },
+    });
+  };
+}
