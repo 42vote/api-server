6862eb15b3b1392f9dd34762ada164f39c6462cb
sindydwns <sindydwns@naver.com>
2023-07-03 01:26:19 +0900

    fix: export excel category

--- src/stat/stat.controller.ts
+++ src/stat/stat.controller.ts
@@ -2,13 +2,12 @@ import {
   BadRequestException,
   Controller,
   Get,
-  Query,
+  Param,
   Res,
   UseGuards,
 } from '@nestjs/common';
 import { StatService } from './stat.service';
 import { AuthAdminGuard } from 'src/auth/auth-admin.guard';
-import * as xlsx from 'xlsx';
 import { Response } from 'express';
 
 @Controller('stat')
@@ -16,22 +15,19 @@ import { Response } from 'express';
 export class StatController {
   constructor(readonly statService: StatService) {}
 
-  @Get('excel/vote')
+  @Get('excel/category/:categoryId')
   async getVoteStat(
     @Res() res: Response,
-    @Query('categoryId') categoryId: number,
+    @Param('categoryId') categoryId: number,
   ) {
     if (Number.isNaN(categoryId)) throw new BadRequestException();
-    const data = await this.statService.getVoteRich({ categoryId });
-    const book = xlsx.utils.book_new();
-    const sheet = xlsx.utils.aoa_to_sheet(data);
-    xlsx.utils.book_append_sheet(book, sheet, 'sheet');
-    const csv = xlsx.write(book, {
-      type: 'string',
-      bookType: 'csv',
-    });
-    res.header('Content-Type', 'text/csv');
-    res.header('Content-Disposition', 'attachment; filename=data.csv');
-    res.end("\uFEFF" + csv);
+    res.header('Content-Type', 'text/excel');
+    res.header('Content-Disposition', 'attachment; filename=data.xlsx');
+    const votes = await this.statService.getVoteRich({ categoryId });
+    const documents = await this.statService.getDocumentRich({ categoryId });
+    this.statService.createExcelStream([
+      { name: 'document', data: documents },
+      { name: 'vote', data: votes },
+    ]).pipe(res);
   }
 }
--- src/stat/stat.service.ts
+++ src/stat/stat.service.ts
@@ -5,27 +5,47 @@ import User from 'src/entity/user.entity';
 import Vote from 'src/entity/vote.entity';
 import Document from 'src/entity/document.entity';
 import Category from 'src/entity/category.entity';
-import SearchStatVoteDto from './dto/search-stat-vote.dto';
+import SearchStatCategoryDto from './dto/search-stat-category.dto';
+import { Readable } from "stream";
+import * as xlsx from 'xlsx';
 
 @Injectable()
 export class StatService {
-  constructor(@InjectRepository(Vote) private voteRepo: Repository<Vote>) {}
+  constructor(
+    @InjectRepository(Vote) private voteRepo: Repository<Vote>,
+    @InjectRepository(Document) private documentRepo: Repository<Document>
+  ) {}
 
-  #getSearchQuery(search: SearchStatVoteDto) {
-    const query = this.voteRepo
-      .createQueryBuilder('vote')
-      .innerJoin('vote.user', 'user')
-      .innerJoin('vote.document', 'document')
-      .innerJoin('document.category', 'category')
-      .innerJoin('document.author', 'author')
-      .where('1=1');
-    if (search.categoryId != null)
-      query.andWhere('category.id = :categoryId', search);
-    return query;
+  createExcelStream(sheetDatas: Array<{name: string, data: any[][]}>) {
+    const book = xlsx.utils.book_new();
+    for (const idx in sheetDatas) {
+      const sheetData = sheetDatas[idx];
+      const sheet = xlsx.utils.aoa_to_sheet(sheetData.data);
+      xlsx.utils.book_append_sheet(book, sheet, sheetData.name);
+    }
+    const buffer = xlsx.write(book, {
+      type: 'buffer',
+    });
+    return this.#bufferToStream(buffer);
   }
 
-  async getVoteRich(search: SearchStatVoteDto): Promise<any[][]> {
-    const query = this.#getSearchQuery(search);
+  #bufferToStream(buffer: any) { 
+    var stream = new Readable();
+    stream.push(buffer);
+    stream.push(null);
+    return stream;
+  }
+
+  async getVoteRich(search: SearchStatCategoryDto): Promise<any[][]> {
+    const query = this.voteRepo
+    .createQueryBuilder('vote')
+    .innerJoin('vote.user', 'user')
+    .innerJoin('vote.document', 'document')
+    .innerJoin('document.category', 'category')
+    .innerJoin('document.author', 'author')
+    .where('1=1');
+    if (search.categoryId != null)
+      query.andWhere('category.id = :categoryId', search);
     query.orderBy('document.title', 'ASC');
     query.setFindOptions({ relations: ['user', 'document'] });
     query.select([
@@ -43,6 +63,40 @@ export class StatService {
         x.document.title,
         x.document.author.intraId,
         x.user.intraId,
+      ]),
+    ];
+  }
+
+  async getDocumentRich(search: SearchStatCategoryDto) {
+    const query = this.documentRepo
+      .createQueryBuilder('document')
+      .innerJoin('document.author', 'author')
+      .innerJoin('document.category', 'category')
+      .innerJoin('document.option', 'option')
+      .leftJoin('document.votes', 'voter')
+      .groupBy('document.id')
+      .where('1=1');
+    if (search.categoryId != null)
+      query.andWhere('category.id = :categoryId', search);
+    query.orderBy('document.title', 'ASC');
+    query.setFindOptions({ relations: ['author', 'category'] });
+    query.select([
+      'document.id',
+      'document.title',
+      'option.goal',
+      'category.title',
+      'author.intraId',
+      'count(voter.userId) as voteCnt',
+    ]);
+    const res = await query.getRawMany();
+    return [
+      ['Category', 'Document', 'Author', 'goal', 'Votes'],
+      ...res.map((x) => [
+        x['category_title'],
+        x['document_title'],
+        x['author_intraId'],
+        x['option_goal'],
+        +x['voteCnt'],
       ]),
     ];
   }
