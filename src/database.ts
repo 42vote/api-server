b2fe0afb58f5adeadd79b3bb5d0edd13ebc7aa96
Yangson Andrew Choi <yachoi@Yangsons-MacBook-Pro.local>
2023-07-31 15:13:17 +0900

    FIX db synchronize true

--- src/database.ts
+++ src/database.ts
@@ -19,7 +19,7 @@ export const createTypeOrmOptions = async (
   username: configService.get<string>('DB_USER'),
   password: configService.get<string>('DB_PASSWORD'),
   database: configService.get<string>('DB_DATABASE'),
-  synchronize: false,
+  synchronize: true,
   timezone: 'Asia/Seoul',
   // loggin: false,
   entities: [
